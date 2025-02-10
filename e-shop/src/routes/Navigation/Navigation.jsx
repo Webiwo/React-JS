bf7e79c3909257b1c7424c0ac1051703fe3baf6a
Webiwo <webiwo360@gmail.com>
2025-02-10 14:03:08 +0100

    Routing, Navigation Bar, SVG configuration

---
+++ e-shop/src/routes/Navigation/Navigation.jsx
@@ -0,0 +1,23 @@
+import { Outlet, Link } from "react-router-dom";
+import ShopLogo from "../../assets/shop-logo.svg?react";
+import "./Navigation.scss";
+
+const Navigation = () => {
+  return (
+    <>
+      <header className="header-bar">
+        <Link className="logo-link" to="/">
+          <ShopLogo className="logo" />
+        </Link>
+        <nav className="nav-links-container">
+          <Link className="nav-link" to="/shop">
+            SHOP
+          </Link>
+        </nav>
+      </header>
+      <Outlet />
+    </>
+  );
+};
+
+export default Navigation;
