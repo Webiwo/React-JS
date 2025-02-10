fdf5939ddffe44bb1ee64d83241ce642121d759f
Webiwo <webiwo360@gmail.com>
2025-02-10 14:59:01 +0100

    added sign in component and firebase utils: firbase, Google popup, db - set and get document

--- e-shop/src/App.jsx
+++ e-shop/src/App.jsx
@@ -2,6 +2,7 @@ import "./App.scss";
 import { Routes, Route } from "react-router-dom";
 import Navigation from "./routes/Navigation/Navigation";
 import Home from "./routes/Home/Home";
+import SignIn from "./routes/SignIn/SignIn";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
       </Route>
+      <Route path="signIn" element={<SignIn />} />
     </Routes>
   );
 };
---
+++ e-shop/src/utils/firebase/firebaseUtils.js
@@ -0,0 +1,44 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+
+
+const firebaseConfig = {
+    apiKey: import.meta.env.VITE_APP_API_KEY,
+    authDomain: import.meta.env.VITE_APP_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_APP_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_APP_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_APP_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_APP_APP_ID,
+};
+
+
+const firebaseApp = initializeApp(firebaseConfig);
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({
+    prompt: "select_account"
+});
+export const auth = getAuth(firebaseApp);
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+export const createUserDocFromAuth = async (userAuth) => {
+    const userDocRef = doc(db, "users", userAuth.uid);
+    const userSnapshot = await getDoc(userDocRef);
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    if (!userSnapshot.exists()) {
+        try {
+            await setDoc(userDocRef, {
+                displayName,
+                email,
+                createdAt
+            })
+        } catch (error) {
+            console.error("error creatin the user", error.message);
+        }
+    }
+
+    return userDocRef;
+}
